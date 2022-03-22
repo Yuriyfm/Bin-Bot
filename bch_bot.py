e66f17a5d0d8e1547be462d7619177a4f705bf2a
Yuriy <yuriyfm@yandex.ru>
2022-03-22 21:57:34 +0700

    change settings

--- bch_bot.py
+++ bch_bot.py
@@ -280,27 +280,26 @@ def check_if_signal(symbol):
         ohlc = get_futures_klines(symbol, 100)
         prepared_df = PrepareDF(ohlc)
         mean_atr = prepared_df[80:95]['ATR'].mean()
-        delta_30 = prepared_df['close'][29] - prepared_df['close'][0]
+        delta_30 = prepared_df['close'][99] - prepared_df['close'][69]
         signal = ""  # return value
 
         i = 98  # 99 - текущая незакрытая свечка, 98 - последняя закрытая свечка, нужно проверить 97-ю росла она или падала
 
-        if isLCC(prepared_df, i - 1) > 0 and prepared_df['close'][0] * -0.007 <= delta_30:
+        if isLCC(prepared_df, i - 1) > 0 and prepared_df['close'][69] * -0.007 <= delta_30:
             # found bottom - OPEN LONG
-            if prepared_df['position_in_channel'][i - 1] < POS_IN_CHANNEL and prepared_df['slope'][i - 1] < -SLOPE and mean_atr < 0.9:
+            if prepared_df['position_in_channel'][i - 1] < POS_IN_CHANNEL and prepared_df['slope'][i - 1] < -SLOPE and mean_atr < 1:
                 # found a good enter point for LONG
                 signal = 'long'
 
-        if isHCC(prepared_df, i - 1) > 0 and delta_30 <= prepared_df['close'][0] * 0.007:
+        if isHCC(prepared_df, i - 1) > 0 and delta_30 <= prepared_df['close'][69] * 0.007:
             # found top - OPEN SHORT
-            if prepared_df['position_in_channel'][i - 1] > 1 - POS_IN_CHANNEL and prepared_df['slope'][i - 1] > SLOPE and mean_atr < 0.9:
+            if prepared_df['position_in_channel'][i - 1] > 1 - POS_IN_CHANNEL and prepared_df['slope'][i - 1] > SLOPE and mean_atr < 1:
                 # found a good enter point for SHORT
                 signal = 'short'
 
         return signal
     except Exception as e:
         prt(f'Ошибка в функции проверки сигнала: \n{e}')
-
 
 telegram_delay = 12
 bot_token = os.getenv("TELEGRAM_TOKEN")
