af100888048b2ee3d1645943f5a81e665e01d1aa
Yuriy <yuriyfm@yandex.ru>
2022-05-22 13:42:47 +0700

    atr stop price

--- ma_bot.py
+++ ma_bot.py
@@ -85,7 +85,8 @@ def main(step):
                 atr_stop_percent = round(get_current_atr(SYMBOL, pointer) / 100, 3)
                 stop_price = entry_price * (1 - atr_stop_percent * 0.5) if STEP_STOP_PRICE is None else STEP_STOP_PRICE
                 if step % 60 == 0:
-                    prt(f'long current stop price: {stop_price}, current price: {current_price}', pointer)
+                    prt(f'Стоп: {stop_price},\nТекущий: {current_price},'
+                        f'\nЦелевой: {entry_price * (1 - atr_stop_percent * 0.5) }', pointer)
                 if current_price < stop_price:
                     # stop loss
                     close_position(SYMBOL, open_sl, round(abs(quantity), 3), atr_stop_percent * 0.5,  pointer)
@@ -113,7 +114,8 @@ def main(step):
                 atr_stop_percent = round(get_current_atr(SYMBOL, pointer) / 100, 3)
                 stop_price = entry_price * (1 + atr_stop_percent * 0.5) if STEP_STOP_PRICE is None else STEP_STOP_PRICE
                 if step % 60 == 0:
-                    prt(f'short current stop price: {stop_price}, current price: {current_price}', pointer)
+                    prt(f'Стоп: {stop_price},\nТекущий: {current_price},'
+                        f'\nЦелевой: {entry_price * (1 - atr_stop_percent * 0.5) }', pointer)
                 if current_price > stop_price:
                     # stop loss
                     close_position(SYMBOL, open_sl, round(abs(quantity), 3), atr_stop_percent * 0.5,  pointer)
