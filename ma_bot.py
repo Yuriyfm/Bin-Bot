5d12ce13293b507140adf60f79193b0d7ee81454
Yuriy <yuriyfm@yandex.ru>
2022-05-22 20:36:16 +0700

    ema + rsi strategy

--- ma_bot.py
+++ ma_bot.py
@@ -16,7 +16,7 @@ SECRET = os.getenv("SECRET")
 SYMBOL = 'ETHUSDT'
 client = Client(KEY, SECRET)
 
-STEP_STOP_PRICE = None
+STOP_PRICE = 0
 pointer = str(f'{SYMBOL}-{random.randint(1000, 9999)}')
 KLINES = 100
 price = get_symbol_price(SYMBOL)
@@ -30,9 +30,10 @@ def main(step):
     global STAT
     global DEAL
     global max_position
+    global STOP_PRICE
 
     current_price = get_symbol_price(SYMBOL)
-
+    atr_stop_percent = round(get_current_atr(SYMBOL, pointer) / 100, 3)
 
     if step == 1:
         prt(f'\nПлюсовых: {STAT["positive"]} '
@@ -55,25 +56,25 @@ def main(step):
 
 
             if signal == 'long':
-                atr_stop_percent = round(get_current_atr(SYMBOL, pointer) / 100, 3)
                 balance = get_wallet_balance()
                 max_position = round(balance * 0.2 / price, 3)
                 now = datetime.datetime.now()
-                open_position(SYMBOL, signal, max_position, atr_stop_percent * 0.5, 3, pointer)
+                open_position(SYMBOL, signal, max_position, atr_stop_percent * 0.35, 3, pointer)
                 DEAL['type'] = signal
                 DEAL['start time'] = now.strftime("%d-%m-%Y %H:%M")
                 DEAL['start price'] = current_price
+                STOP_PRICE = current_price * (1 - atr_stop_percent * 0.35)
                 prt(f'Открыл {signal} {max_position}{SYMBOL} на {round(max_position * current_price, 2)}$, по курсу {current_price}', pointer)
 
             elif signal == 'short':
-                atr_stop_percent = round(get_current_atr(SYMBOL, pointer) / 100, 3)
                 balance = get_wallet_balance()
                 max_position = round(balance * 0.2 / price, 3)
                 now = datetime.datetime.now()
-                open_position(SYMBOL, signal, max_position, atr_stop_percent * 0.5, 3, pointer)
+                open_position(SYMBOL, signal, max_position, atr_stop_percent * 0.35, 3, pointer)
                 DEAL['type'] = signal
                 DEAL['start time'] = now.strftime("%d-%m-%Y %H:%M")
                 DEAL['start price'] = current_price
+                STOP_PRICE = current_price * (1 + atr_stop_percent * 0.35)
                 prt(f'Открыл {signal} {max_position}{SYMBOL} на {round(max_position * current_price, 2)}$, по курсу {current_price}', pointer)
 
         else:
@@ -82,15 +83,15 @@ def main(step):
 
 
             if open_sl == 'long':
-                atr_stop_percent = round(get_current_atr(SYMBOL, pointer) / 100, 3)
-                stop_price = entry_price * (1 - atr_stop_percent * 0.5) if STEP_STOP_PRICE is None else STEP_STOP_PRICE
+                if current_price * (1 - atr_stop_percent * 0.35) > STOP_PRICE:
+                    STOP_PRICE = current_price * (1 - atr_stop_percent * 0.35)
                 if step % 60 == 0:
-                    prt(f'long\nСтоп: {round(stop_price, 2)},\nТекущий: {current_price},'
-                        f'\nЦелевой: {round(entry_price * (1 + atr_stop_percent * 0.5), 2)}', pointer)
-                if current_price < stop_price:
+                    prt(f'long\nВход: {entry_price}\n\nТекущая: {current_price},\nСтоп: {round(STOP_PRICE, 2)},'
+                        f'\nТекущий %:{round((current_price /  entry_price - 1) * 100, 2)}', pointer)
+                if current_price < STOP_PRICE:
                     # stop loss
-                    close_position(SYMBOL, open_sl, round(abs(quantity), 3), atr_stop_percent * 0.5,  pointer)
-                    profit = round(((current_price / entry_price - 1) * 100) - 0.04, 3)
+                    close_position(SYMBOL, open_sl, round(abs(quantity), 3), atr_stop_percent * 0.35,  pointer)
+                    profit = round(((current_price / entry_price - 1) * 100) - 0.045, 3)
                     if profit > 0:
                         STAT['positive'] += 1
                     else:
@@ -102,24 +103,17 @@ def main(step):
                     STAT['deals'].append(DEAL)
                     DEAL = {}
                     STEP_STOP_PRICE = None
-                else:
-                    if entry_price * (1 + atr_stop_percent * 0.5) < current_price:
-                        if not STEP_STOP_PRICE:
-                            STEP_STOP_PRICE = current_price * (1 - atr_stop_percent * 0.3)
-                        else:
-                            if current_price * (1 - atr_stop_percent * 0.3) > STEP_STOP_PRICE:
-                                STEP_STOP_PRICE = current_price * (1 - atr_stop_percent * 0.3)
 
             if open_sl == 'short':
-                atr_stop_percent = round(get_current_atr(SYMBOL, pointer) / 100, 3)
-                stop_price = entry_price * (1 + atr_stop_percent * 0.5) if STEP_STOP_PRICE is None else STEP_STOP_PRICE
+                if current_price * (1 + atr_stop_percent * 0.35) < STOP_PRICE:
+                    STOP_PRICE = current_price * (1 + atr_stop_percent * 0.35)
                 if step % 60 == 0:
-                    prt(f'short\nСтоп: {round(stop_price, 2)},\nТекущий: {current_price},'
-                        f'\nЦелевой: {round(entry_price * (1 - atr_stop_percent * 0.5), 2)}', pointer)
-                if current_price > stop_price:
+                    prt(f'short\nВход: {entry_price}\n\nТекущая: {current_price},\nСтоп: {round(STOP_PRICE, 2)},'
+                        f'\nТекущий %:{round((1 - current_price /  entry_price) * 100, 2)}', pointer)
+                if current_price > STOP_PRICE:
                     # stop loss
-                    close_position(SYMBOL, open_sl, round(abs(quantity), 3), atr_stop_percent * 0.5,  pointer)
-                    profit = round(((current_price / entry_price - 1) * -100) - 0.04, 3)
+                    close_position(SYMBOL, open_sl, round(abs(quantity), 3), atr_stop_percent * 0.35,  pointer)
+                    profit = round(((current_price / entry_price - 1) * -100) - 0.045, 3)
                     if profit > 0:
                         STAT['positive'] += 1
                     else:
@@ -131,13 +125,6 @@ def main(step):
                     STAT['deals'].append(DEAL)
                     DEAL = {}
                     STEP_STOP_PRICE = None
-                else:
-                    if entry_price * (1 - atr_stop_percent * 0.5) > current_price:
-                        if not STEP_STOP_PRICE:
-                            STEP_STOP_PRICE = current_price * (1 + atr_stop_percent * 0.3)
-                        else:
-                            if current_price * (1 + atr_stop_percent * 0.3) < STEP_STOP_PRICE:
-                                STEP_STOP_PRICE = current_price * (1 + atr_stop_percent * 0.3)
 
     except Exception as e:
         prt(f'Ошибка в main: \n{e}', pointer)
