90dd97365fd7681989810ade4f66329e18ca7775
Yuriy <yuriyfm@yandex.ru>
2022-07-05 23:19:28 +0700

    multibot

--- functions.py
+++ functions.py
@@ -304,7 +304,7 @@ def get_last_intersection(DF, SMA_1, SMA_2):
     if trend:
         return trend, len(DF) - ints_list, DF['open'][ints_list]
     else:
-        'no intersections'
+        ['no intersections']
 
 
 def check_diff(pointer, SYMBOL_LIST):
@@ -312,18 +312,20 @@ def check_diff(pointer, SYMBOL_LIST):
         symbol_list = parce_val() if SYMBOL_LIST == [] else SYMBOL_LIST
         for i in symbol_list:
             DF = get_futures_klines(i, 100, pointer, 1)
-            DF = prepareDF(DF)
-            DF['SMA_9'] = get_sma(DF['close'], 9)
-            DF['SMA_31'] = get_sma(DF['close'], 31)
-            DF = get_rsi(DF)
-            DF = get_bollinger_bands(DF)
+            if DF:
+                DF = prepareDF(DF)
+                DF['SMA_9'] = get_sma(DF['close'], 9)
+                DF['SMA_31'] = get_sma(DF['close'], 31)
+                DF = get_rsi(DF)
+                DF = get_bollinger_bands(DF)
 
-            res = get_last_intersection(DF, 9, 31)
-            if res[0] == 'long':
-                cur_price = get_symbol_price(i)
-                if 1 - res[2] / cur_price >= 0.5 and DF['RSI'][-1] > 70 and DF['close'][-1] > DF['upper_band'][-1]:
-                    prt(f'выбрал валюту {i}', pointer)
-                    return i
+                res = get_last_intersection(DF, 9, 31)
+
+                if res[0] == 'long':
+                    cur_price = get_symbol_price(i)
+                    if cur_price and 1 - res[2] / cur_price >= 0.5 and DF['RSI'][-1] > 70 and DF['close'][-1] > DF['upper_band'][-1]:
+                        prt(f'выбрал валюту {i}', pointer)
+                        return i
         return ''
     except Exception as e:
         prt(f'Ошибка в функции выбора валюты: \n{e}', pointer)
--- test_bot.py
+++ test_bot.py
@@ -3,18 +3,10 @@ from functions import *
 import json
 import datetime
 
-
-
-
 load_dotenv()
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 KEY = os.getenv("KEY")
-SYMBOL = ''
-
-
-if SYMBOL:
-    print('hey')
 
 
 
